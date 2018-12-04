9dcd3d7ebad7419b7640199a57b247827e99db96
Olga Giza <olga.giza@sabio.de>
2018-12-04 22:05:53 +0100

    Add `enzyme` for component tests.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,19 @@
+import { mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Application view', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('renders search component', () => {
+        const app = mount(<App />);
+
+        expect(app.find('.Search').exists()).toBeTruthy();
+    });
 });
